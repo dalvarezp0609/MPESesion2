0fa2633015101bcc9c7cc45cbdc4484f5990e083
dalvarezp0609 <dalvarezp@uamv.edu.ni>
2024-08-26 10:37:54 -0600

    clase 26/08/2024

---
+++ Sesion3/Modelo/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion3.Modelo
+{
+    internal class Persona
+    {
+        public Persona() { }
+        public string Nombre { get; set; }
+        public DateTime FechaNac { get; set; }
+        public int CalcularEdad()
+        {
+            int edad = 0;
+            try
+            {
+                edad = DateTime.Now.Year - FechaNac.Year;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ocurrio un error: { ex.Message}");
+                
+            }
+            return edad;
+        }
+        public string EvaluarEdad()
+        {
+            if (CalcularEdad() >= 18) return "Mayor de edad";
+            return "menor de edad";
+        }
+
+    }
+}
---
+++ Sesion3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Sesion3.Modelo;
+
+namespace Sesion3
+{
+    internal class Program
+    {
+        /* calcular la edad de una persona y decir si es mayor o menor de edad*/
+         
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string nombre;
+            DateTime fechaNac;
+            Console.WriteLine("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Fecha de nacimiento: " );
+            fechaNac = DateTime.Parse(Console.ReadLine());
+            persona.Nombre = nombre;
+            persona.FechaNac = fechaNac;
+            Console.WriteLine(persona.EvaluarEdad());
+        }
+    }
+}
